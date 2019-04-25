2283ad561ac6c2a8d759c615ef69bc9b1fdcfd28
Philippe Coval <p.coval@samsung.com>
2019-04-25 19:00:23 +0200

    WIP: mod: rules/stm32/rules.mk (sandbox/rzr/devel/master)
    Change-Id: Ib8c8e6e40dc5e6cca55b65d15ef9a8ae4152c001
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

--- rules/stm32/rules.mk
+++ rules/stm32/rules.mk
@@ -1,8 +1,9 @@
 # TODO: keep private in ~/
 dev_id?=066EFF323535474B43065221
 dev_file?=/dev/disk/by-id/usb-MBED_microcontroller_${dev_id}-0:0
+dev_link?=/tmp/${CURDIR}/deploy
 #USER?=$(shell echo ${USER})
-nuttx_deploy_dir?=/media/${USER}/NODE_F767ZI1/
+nuttx_deploy_dir?=/media/${USER}/NODE_F767ZI1
 nuttx_dur?=.
 nuttx_image_file?=nuttx.bin
 monitor_rate?=115200
@@ -17,19 +18,23 @@ todo/stm32/deploy: ${nuttx_image_file}
 	cp $< /media/philippe/NODE_F767ZI1/
 # 0483:374b STMicroelectronics ST-LINK/V2.1 (Nucleo-F103RB)
 
-${nuttx_deploy_dir}:
+${dev_link}/DETAILS.TXT:
 	ls -l ${dev_file}
 	sudo umount -f ${dev_file} ${deploy_dir} || echo $$?
 	udisksctl mount -b ${dev_file} ||:
-	ls $@
+	ln -fs ${deploy_dir} ${@D}
+
+${nuttx_deploy_dir}: ${dev_link}/DETAILS.TXT
+	ls ${@}
 
 ${nuttx_deploy_dir}/nuttx.bin: ${nuttx_image_file}  ${nuttx_deploy_dir}
 	cp -av $< $@
+	sleep 6
 	sudo sync
-	ls @
+#	ls ${@D}
 
 stm32/deploy: ${nuttx_deploy_dir}/nuttx.bin
-	ls $<
+	sudo sync
 
 ${monitor_file}:
 	lsusb
